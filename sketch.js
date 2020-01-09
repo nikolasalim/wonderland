7504c50ae9fc0434b71592af016da081c1a02ef2
nikolasalim <nikolasalim@gmail.com>
2020-01-09 13:43:22 +0100

    first commit

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let x;
+let y;
+
+let xSpeed;
+let ySpeed;
+
+let intro = "oh, hi! I'm Niko and that's my head over there, bouncing like a dvd logo."
+
+function preload(){
+	head = loadImage("head.png");
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	x = 0;
+	y = 0;
+	xSpeed = 5;
+	ySpeed = 5;
+	colors = ['White', 'DodgerBlue', 'SpringGreen', 'Coral', 'Crimson', 'Fuchsia', 'Yellow', 'Aqua'];
+	tint(random(colors))
+}
+
+function draw() {
+	background(0)	
+	image(head, x, y, 130, 200)
+
+	x += xSpeed;
+	y += ySpeed;
+
+	if(x + 130 >= windowWidth || x <= 0){
+		xSpeed = -xSpeed;
+		tint(random(colors));
+	}	
+	if(y + 200 >= windowHeight || y <= 0){
+		ySpeed = -ySpeed;
+		tint(random(colors));
+	}		
+};
