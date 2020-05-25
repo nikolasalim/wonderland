cceb002e34782cc4a16063687eceb1882467dcc0
nikolasalim <nikolasalim@gmail.com>
2020-05-25 17:28:34 +0200

    add questions and answers

--- sketch.js
+++ sketch.js
@@ -4,35 +4,42 @@ let y;
 let xSpeed;
 let ySpeed;
 
-let intro = "oh, hi! I'm Niko and that's my head over there, bouncing like a dvd logo."
-
-function preload(){
-	head = loadImage("head.png");
+function preload() {
+  head = loadImage("head.png");
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	x = 0;
-	y = 0;
-	xSpeed = 5;
-	ySpeed = 5;
-	colors = ['White', 'DodgerBlue', 'SpringGreen', 'Coral', 'Crimson', 'Fuchsia', 'Yellow', 'Aqua'];
-	tint(random(colors))
+  createCanvas(windowWidth, windowHeight);
+  x = 0;
+  y = 0;
+  xSpeed = 5;
+  ySpeed = 5;
+  colors = [
+    "White",
+    "DodgerBlue",
+    "SpringGreen",
+    "Coral",
+    "Crimson",
+    "Fuchsia",
+    "Yellow",
+    "Aqua",
+  ];
+  tint(random(colors));
 }
 
 function draw() {
-	background(0)	
-	image(head, x, y, 130, 200)
+  background(0);
+  image(head, x, y, 130, 200);
 
-	x += xSpeed;
-	y += ySpeed;
+  x += xSpeed;
+  y += ySpeed;
 
-	if(x + 130 >= windowWidth || x <= 0){
-		xSpeed = -xSpeed;
-		tint(random(colors));
-	}	
-	if(y + 200 >= windowHeight || y <= 0){
-		ySpeed = -ySpeed;
-		tint(random(colors));
-	}		
-};
+  if (x + 130 >= windowWidth || x <= 0) {
+    xSpeed = -xSpeed;
+    tint(random(colors));
+  }
+  if (y + 200 >= windowHeight || y <= 0) {
+    ySpeed = -ySpeed;
+    tint(random(colors));
+  }
+}
