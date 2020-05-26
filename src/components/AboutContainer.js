096f056eb7e4cea464f04aac7b1d24a9b5bd159d
nikolasalim <nikolasalim@gmail.com>
2020-05-26 11:58:07 +0200

    fix git

--- src/components/AboutContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import About from "./About";
-
-function AboutContainer() {
-  return <About />;
-}
-
-export default AboutContainer;
--- src/components/BioContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Bio from "./Bio";
-
-function BioContainer() {
-  return <Bio />;
-}
-
-export default BioContainer;
